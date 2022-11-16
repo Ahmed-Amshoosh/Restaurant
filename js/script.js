5e83f1cab5df4d05d149a2036ba877b88f54b588
Ahmed-Amshoosh <103959192+Ahmed-Amshoosh@users.noreply.github.com>
2022-11-16 20:13:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let navbar=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = ()=>{
+    navbar.classList.toggle('active');
+}
+
+
+
+
+let searchForm =document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = ()=>{
+    searchForm.classList.toggle('active');
+}
+
+
+
